267803ba03e8e4d02987c4760e7a7360d4a1725c
suha1028 <77815796+suha1028@users.noreply.github.com>
2021-02-07 15:46:02 -0700

    requirements file and updated read me

--- game.py
+++ game.py
@@ -3,11 +3,11 @@
 import random
 
 #customize player name
+
 import os
 import dotenv
 dotenv.load_dotenv()
 PLAYER_NAME = os.getenv("PLAYER_NAME", default = "Player One")
-
 print("-------------------")
 print(F"Welcome '{PLAYER_NAME}' to my Rock-Paper-Scissors game...")
 print("-------------------")
@@ -16,26 +16,26 @@ print("-------------------")
 
 user_choice =input("Please choose either 'rock', 'paper', or 'scissors': ")
 options = ["rock", "paper", "scissors"]
+
 #print(x)
 
 print("You chose:",user_choice)
 
-user_choice.lower()
+user_choice = user_choice.lower()
 
 if user_choice not in options:
-    print("OOPS, please choose a valid option and try again")
+    print("OOPS, please choose a valid option and try again!")
     exit()
-
 
 #simulating a computer input
 
 computer_choice = random.choice(options)
-
 print(f"The computer chose: {computer_choice}")
-
+print("-------------------")
     
 #determining the winner
 # adapted from Estelle's solution shared in the opim143 slack channel
+
 if computer_choice == "paper" and user_choice == "scissors":
     print("Yay! You won!")
 elif computer_choice == "rock" and user_choice == "paper":
@@ -47,7 +47,7 @@ elif computer_choice == user_choice:
 else:
     print("Oh, the computer won. It's ok.")
 
-print("-------------------")
+#ending message
 
 print("-------------------")
 print("Thanks for playing. Please play again!")
