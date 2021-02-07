f05974bd60eeac10797140e985e4874d654dee31
suha1028 <77815796+suha1028@users.noreply.github.com>
2021-02-07 14:55:22 -0700

    displaying results

--- game.py
+++ game.py
@@ -30,16 +30,21 @@ computer_choice = random.choice(options)
 
 print(f"The computer chose: {computer_choice}")
 
-
-
-
-
     
 #determining the winner
-
-
+# adapted from Estelle's solution shared in the opim143 slack channel
+if computer_choice == "paper" and user_choice == "scissors":
+    print("Yay! You won!")
+elif computer_choice == "rock" and user_choice == "paper":
+    print("Yay! You won!")
+elif computer_choice == "scissors" and user_choice == "rock":
+    print("Yay! You won!")
+elif computer_choice == user_choice:
+    print("It's a tie!")
+else:
+    print("Oh, the computer won. It's ok.")
 
 print("-------------------")
-print("Oh, the computer won. It's ok.")
+
 print("-------------------")
 print("Thanks for playing. Please play again!")
